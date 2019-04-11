a52e6098f8e3f5674c683dd3c955e49c8c49d3a6
mattdamon108 <woonki.moon@gmail.com>
2019-04-11 16:07:43 +0900

    fix GetMyClientsQueryArgs

--- src/resolvers/Client/Query/getMyClients.resolvers.ts
+++ src/resolvers/Client/Query/getMyClients.resolvers.ts
@@ -1,11 +1,12 @@
 import { Rate_client } from "../../../../generated/prisma-client";
+import { GetMyClientsQueryArgs } from "../../../types/graph";
 import { ContextWithUser } from "../../../types/resolver";
 import privateResolver from "../../../util/privateResolver";
 
 export default {
   Query: {
     getMyClients: privateResolver(
-      async (_: any, args: any, ctx: ContextWithUser): Promise<Rate_client[]> => {
+      async (_: any, args: GetMyClientsQueryArgs, ctx: ContextWithUser): Promise<Rate_client[]> => {
         try {
           const clients = await ctx.prisma.rate_clients({
             orderBy: "name_ASC",
